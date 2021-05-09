41953c519c8cd6aa4a51cfe1bb6c5a3ed6017edd
Jerry <47512608+Jerry-Licious@users.noreply.github.com>
2021-05-08 23:23:46 -0400

    Used setMove patch to phase out using a game effect.

--- src/main/java/domemod/patches/MonsterSetMovePatch.kt
+++ src/main/java/domemod/patches/MonsterSetMovePatch.kt
@@ -4,8 +4,9 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePatch
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch
 import com.megacrit.cardcrawl.monsters.AbstractMonster
 
-@SpirePatch(clz = AbstractMonster::class, method = "applyPowers")
-class MonsterApplyPowersPatch {
+@SpirePatch(clz = AbstractMonster::class, method = "setMove", paramtypez = [String::class, Byte::class,
+    AbstractMonster.Intent::class, Int::class, Int::class, Boolean::class])
+class MonsterSetMovePatch {
     companion object {
         @JvmStatic
         @SpirePostfixPatch
