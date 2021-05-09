c61128623066dc3238988bfb6a0c95180951be68
Jerry <47512608+Jerry-Licious@users.noreply.github.com>
2021-05-08 23:08:36 -0400

    Changed the update intents action to be a game effect instead. Fixes #1.

--- src/main/java/domemod/patches/MonsterApplyPowersPatch.kt
+++ src/main/java/domemod/patches/MonsterApplyPowersPatch.kt
@@ -1,10 +1,11 @@
 package domemod.patches
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch
-import com.megacrit.cardcrawl.actions.AbstractGameAction
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon
 import com.megacrit.cardcrawl.monsters.AbstractMonster
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect
 
 @SpirePatch(clz = AbstractMonster::class, method = "applyPowers")
 class MonsterApplyPowersPatch {
@@ -12,7 +13,10 @@ class MonsterApplyPowersPatch {
         @JvmStatic
         @SpirePostfixPatch
         fun updateIntents(monster: AbstractMonster) {
-            AbstractDungeon.actionManager.addToBottom(object: AbstractGameAction() {
+            AbstractDungeon.effectsQueue.add(object: AbstractGameEffect() {
+                override fun dispose() {}
+                override fun render(sb: SpriteBatch?) {}
+
                 override fun update() {
                     monster.moveSet.updateIntents(monster)
                     isDone = true
