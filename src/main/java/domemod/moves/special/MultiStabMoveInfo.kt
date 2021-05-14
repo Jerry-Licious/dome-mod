3b2218794fa9e672b1c5ee615d36f2a896c58504
Jerry <47512608+Jerry-Licious@users.noreply.github.com>
2021-05-14 11:23:15 -0400

    It is now impossible to guess book of stabbing's next move based on the stab count displayed by dome mod.

--- src/main/java/domemod/moves/special/MultiStabMoveInfo.kt
+++ src/main/java/domemod/moves/special/MultiStabMoveInfo.kt
@@ -1,15 +1,20 @@
 package domemod.moves.special
 
-import basemod.ReflectionHacks
+import com.megacrit.cardcrawl.actions.GameActionManager
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon
 import com.megacrit.cardcrawl.monsters.AbstractMonster
-import com.megacrit.cardcrawl.monsters.city.BookOfStabbing
 import domemod.moves.MoveInfo
 
 class MultiStabMoveInfo(baseDamage: Int): MoveInfo(AbstractMonster.Intent.ATTACK, 1, baseDamage, 2) {
     override fun updateIntent(monster: AbstractMonster) {
         super.updateIntent(monster)
-        if (monster is BookOfStabbing) {
-            multiplier = ReflectionHacks.getPrivate(monster, BookOfStabbing::class.java, "stabCount")
+        if (AbstractDungeon.ascensionLevel >= 18){
+            // If it is the enemies' turn, the move is queued before the player's turn starts, so an offset of 1 is added.
+            multiplier = if (AbstractDungeon.actionManager.turnHasEnded) { GameActionManager.turn + 2 }
+                else { GameActionManager.turn + 1 }
+        } else {
+            multiplier = (monster.moveHistory.reversed().stream().skip(1)
+                    .filter { it == (1).toByte() }.count() + 2).toInt()
         }
     }
 
