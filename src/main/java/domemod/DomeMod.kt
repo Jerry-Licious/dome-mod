e92fa277a0b853a52cf4aa01974dfaf8e98f7227
Jerry <47512608+Jerry-Licious@users.noreply.github.com>
2021-05-14 12:20:29 -0400

    Added mod icon and badge.

--- src/main/java/domemod/DomeMod.kt
+++ src/main/java/domemod/DomeMod.kt
@@ -1,7 +1,9 @@
 package domemod
 
 import basemod.BaseMod
+import basemod.ModPanel
 import basemod.interfaces.PostInitializeSubscriber
+import com.badlogic.gdx.graphics.Texture
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer
 import domemod.moves.MonsterDatabase
 import org.apache.logging.log4j.LogManager
@@ -24,5 +26,9 @@ class DomeMod: PostInitializeSubscriber {
 
     override fun receivePostInitialize() {
         MonsterDatabase.load()
+
+        BaseMod.registerModBadge(Texture("domemod/icon.png"), "Dome Visualised", "Jerry",
+                "Slay the Spire mod that displays all possible moves of all enemies while in combat when " +
+                        "you have Runic Dome. ", ModPanel())
     }
 }
